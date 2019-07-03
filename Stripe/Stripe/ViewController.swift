0e498944c46945a1f09af7682266e3ccdb74f23a
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-07-02 22:31:54 -0400

    add starter Xcode project

---
+++ Stripe/Stripe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stripe
+//
+//  Created by Julian A. Fordyce on 7/2/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
